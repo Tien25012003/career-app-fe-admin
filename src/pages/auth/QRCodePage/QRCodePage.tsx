20cd94dd210665934dfcced6c279d120e07c7915
Tien25012003 <nguyenthiphuongtien12e@gmail.com>
2025-04-19 15:19:58 +0700

    Test | Test Deeplink

--- src/pages/auth/QRCodePage/QRCodePage.tsx
+++ src/pages/auth/QRCodePage/QRCodePage.tsx
@@ -1,20 +1,27 @@
 import { getMobileOperatingSystem } from '@util/PlatformUtil';
 import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 export const QRCodePage = () => {
   const QR_CODE = 'm_92';
+
+  const [searchParams] = useSearchParams();
+  const param = searchParams.get('scanResult');
+
+  console.log('param', param);
+
   // EFFECT
   useEffect(() => {
     const os = getMobileOperatingSystem();
     if (os === 'Android') {
       // window.location.href = `intent://career-app-fe-admin.vercel.app?qr-code=${QR_CODE}#Intent;scheme=https;package=com.capsulekaiba.pickleduels;end;`;
-      window.location.href = `intent://career-app-fe-admin.vercel.app?qr-code=${QR_CODE}#Intent;scheme=https;package=com.capsulekaiba.pickleduels;end;`;
+      window.location.href = `intent://career-app-fe-admin.vercel.app?scanResult=${param}#Intent;scheme=https;package=com.capsulekaiba.pickleduels;end;`;
     } else if (os === 'iOS') {
       console.log('IOS');
     } else {
       window.location.href = 'https://www.capsulekaiba.com/about-us';
     }
-  }, []);
+  }, [param]);
 
   return (
     <div
